a8b008529a2adae24fd3f3d6bf5f6a3b927b66f7
JulioYauri <jyaurii@unsa.edu.pe>
2026-06-16 03:31:34 -0500

    after contest

--- virtuals/NAC2025/L.cpp
+++ virtuals/NAC2025/L.cpp
@@ -23,24 +23,22 @@ ll max_with(ll take, ll len, ll r, ll other_len) {
     return other_mx; 
 }   
 void solve() { 
-    ll r, w, h; cin >> r >> w >> h; 
-    if(w > h) swap(w, h); 
+    ll r, w, h; cin >> r >> w >> h;
+    auto f = [&](ll x) { 
+        ll y = max_with(x / h, h, r, w); 
+        return x / h * y ; 
+    }; 
 
-    auto f = [&](ll take) { 
-        ll mx = max_with(take, h, r, w);
-        return mx * take;  
-        // ll nxt_mx = max_with(mx, w, r, h);
-        // return mx * nxt_mx; 
-    };
-
-    ll a = 1, b = 2 * r / h; 
-    while (b - a >= 5) {
-		ll mid = a + (b - a) / 2;
-		if (f(mid) < f(mid+1)) a = mid; // (A)
-		else b = mid+1;
-	}
-	rep(i,a+1,b+1) if (f(a) < f(i)) a = i; // (B)
-    cout << f(a) << "\n"; 
+    ll ans = 0; 
+    rep(_,0,2) { 
+        ll x = sqr(2LL * r * r);
+        ll x_l = x / h * h; 
+        for(int its = 0; its < 1000 && x_l > 0; its++, x_l -= h) ans = max(ans, f(x_l)); 
+        ll x_r = x_l + h; 
+        for(int its = 0; its < 1000 && x_r <= 2 * r; its++, x_r += h) ans = max(ans, f(x_r)); 
+        swap(w, h); 
+    } 
+    cout << ans << "\n"; 
 }
 
 int main(){
