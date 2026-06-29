d74db9a73c2491ef17a84b09df39ee92bf1e1285
JulioYauri <jyaurii@unsa.edu.pe>
2026-06-29 15:52:37 -0500

    Crimson Triples

---
+++ CodeForces/2238B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll; 
+typedef vector<ll> vl; 
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<pii> vii;
+
+
+void solve() {
+    int n; cin >> n; 
+    ll ans = 0; 
+    rep(b,1,n+1) { 
+        ll q = n / b; 
+        ans += q * q; 
+    }
+    cout << ans << "\n"; 
+}
+
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int tt; cin >> tt; 
+	while(tt--) solve();
+}
