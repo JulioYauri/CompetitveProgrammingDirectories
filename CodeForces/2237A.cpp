ad2fc38479a7c9e06687c3bcf05cc6aa2f7d1f95
JulioYauri <jyaurii@unsa.edu.pe>
2026-07-02 15:16:43 -0500

    Destroying Towers

---
+++ CodeForces/2237A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll; 
+typedef vector<ll> vl; 
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<pii> vii;
+
+
+void solve() {
+	int n; cin >> n; 
+    int ans = 0, cur_min = 100000; 
+    rep(i,0,n) { 
+        int x; cin >> x; 
+        cur_min = min(cur_min, x); 
+        ans += cur_min; 
+    }
+    cout << ans << "\n"; 
+}
+
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int tt; cin >> tt; 
+	while(tt--) solve();
+}
